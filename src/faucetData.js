f0e24364e68b78805056e14ac77bf7fc4827c11e
胡云辉 <2179709677@qq.com>
2023-04-06 14:09:46 +0800

    新增水龙头

--- src/faucetData.js
+++ src/faucetData.js
@@ -79,6 +79,10 @@ export function faucetData(that) {
                 {
                     "url": "https://mumbaifaucet.com/",
                     "remark": "每天0.5 Mumbai MATIC"
+                },
+                {
+                    "url": "https://faucet.polygon.technology",
+                    "remark": ""
                 }
             ]
         },
@@ -97,6 +101,101 @@ export function faucetData(that) {
                     "url": "https://testnet.bnbchain.org/faucet-smart",
                     "remark": ""
                 }
+            ]
+        },
+        {
+            "network": "Gnosis Chain",
+            "remark": "Gnosis 测试网",
+            "chainID": "300",
+            "currency": "xDai",
+            "img": "gnosisLogo.webp",
+            "faucet": [
+                {
+                    "url": "https://stakely.io/faucet/xdai-chain",
+                    "remark": ""
+                }
+            ]
+        },
+        {
+            "network": "Avalanche Testnet",
+            "remark": "Avalanche 测试网",
+            "chainID": "43113",
+            "currency": "AVAX",
+            "img": "avalancheLogo.webp",
+            "faucet": [
+                {
+                    "url": "https://faucet.avax-test.network",
+                    "remark": ""
+                }
+            ]
+        },
+        {
+            "network": "OKExChain Testnet",
+            "remark": "OKExChain 测试网",
+            "chainID": "65",
+            "currency": "OKT",
+            "img": "okexLogo.webp",
+            "faucet": [
+                {
+                    "url": "https://gitter.im/kovan-testnet/faucet",
+                    "remark": ""
+                }
+            ]
+        },
+        {
+            "network": "Harmony Testnet",
+            "remark": "Harmony 测试网",
+            "chainID": "1666700000",
+            "currency": "ONE",
+            "img": "harmonyLogo.webp",
+            "faucet": [
+                {
+                    "url": "https://faucet.pops.one",
+                    "remark": "包含Shard 0、Shard 1、Shard 2、Shard 3"
+                }
+            ]
+        },
+        {
+            "network": "Aurora Testnet",
+            "remark": "Aurora 测试网",
+            "chainID": "1313161555",
+            "currency": "ETH",
+            "img": "auroraLogo.webp",
+            "faucet": [
+                {
+                    "url": "https://aurora.dev/faucet",
+                    "remark": ""
+                }
+            ]
+        },
+        {
+            "network": "Celo Alfajores Testnet",
+            "remark": "Celo 测试网",
+            "chainID": "44787",
+            "currency": "CELO",
+            "img": "celoLogo.webp",
+            "faucet": [
+                {
+                    "url": "https://faucet.celo.org/",
+                    "remark": "需要使用Github进行身份验证"
+                }
+            ]
+        },
+        {
+            "network": "Cube Chain Testnet",
+            "remark": "Cube 测试网",
+            "chainID": "1819",
+            "currency": "CUBET",
+            "img": "cubeLogo.png",
+            "faucet": [
+                {
+                    "url": "https://faucet.celo.org/",
+                    "remark": "需要使用Github进行身份验证"
+                },
+                {
+                    "url": "https://faucet.cube.network/",
+                    "remark": "需要使用Github进行身份验证"
+                },
             ]
         }
     ]
